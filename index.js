d1a38387db9ea1767c3603725db0e7373ca2297f
saikrishna <saikrishna.4369@gmail.com>
2022-07-14 16:51:39 +0200

    setting up Db and some small configs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import * as constant from "./setupSupport/constants.js";
 const express = require("express");
 const passport = require("passport");
 const mongoose = require("mongoose");
@@ -6,10 +7,15 @@ const applicationPortNumber = 3000;
 
 const application = express();
 
+await mongoose.connect(constant.mongoURL).then(() => {
+    console.log("The DB has connected successfully")
+}).catch(err => console.log(err));
+
+
 application.get("/", (req, res) => {
     res.send("Welcome to user registration application :)");
-})
+});
 
 application.listen(applicationPortNumber, () => {
     console.log(`Application is listening on port ${applicationPortNumber}`);
-})
+});
---
+++ setupSupport/constants.js
@@ -0,0 +1,2 @@
+export const mongoURL = "mongodb+srv://m001-student:m001-mongodb-basics@<sandbox.r05hnxv.mongodb.net/myFirstDatabase>"
+export const secret = "myStrongSecretCode"
