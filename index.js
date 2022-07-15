13aa4910fde9980485c9a84baddcd4a4beeafbb5
saikrishna <saikrishna.4369@gmail.com>
2022-07-15 16:28:31 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import User from "./models/UserRegister.js";
 import * as constant from "./setupSupport/constants.js";
 const express = require("express");
 const passport = require("passport");
 const mongoose = require("mongoose");
+const authorization = require("./routes/api/Auth");
 
 const applicationPortNumber = 3000;
 
@@ -11,11 +13,17 @@ await mongoose.connect(constant.mongoURL).then(() => {
     console.log("The mongoDB has connected successfully");
 }).catch(error => console.log(error));
 
+application.use("/register", authorization);
 
 application.get("/", (req, res) => {
     res.send("Welcome to user registration application :)");
 });
 
+application.post("/register", (req, res) => {
+    
+});
+
 application.listen(applicationPortNumber, () => {
     console.log(`Application is listening on port ${applicationPortNumber}`);
 });
+
