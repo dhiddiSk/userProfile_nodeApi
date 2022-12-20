cbff264e2202c342db54bfa9f05cddc8bee1294b
saikrishna <saikrishna.4369@gmail.com>
2022-12-20 12:25:02 +0530

    update

--- tests/routes/api/Auth.test.ts
+++ tests/routes/api/Auth.test.ts
@@ -1,18 +1,26 @@
 import {describe, expect, test} from '@jest/globals';
 import {generateHashPassword, updatePasswordInDatabase, registration} from '../../../src/api/routes/api/Auth'
-
+import { UserRegistration } from '../../../src/api/models/UserRegisterSchema'
 
 describe('Authentication module tests', () => {
+    let newPassword = "saiKrishna";
+    let userTestEmail = "test@test.com";
+    let responceObject;
     test('generateHashPassword function', () => {
-      // Send arguments to this method and validate if the hash of the
-      // passwords are getting saved in the database
+    // Send arguments to this method and validate if the hash of the passwords are getting saved in the database.
+    // Send the arguments like (UserRegistration, newPassword, userEmail, res).
+    // Pass the arguments to the method and check if the method is storing the hashed passwords.
+    generateHashPassword(UserRegistration, newPassword, userTestEmail, responceObject);
+    
     });
 
     test('updatePasswordInDatabase function', () => {
-        // Send the arguments to this method and validate if the password is getting updated in the database
+        // Send the arguments to this method and validate if the password is getting updated in the database.
+        updatePasswordInDatabase(userTestEmail, UserRegistration, newPassword, responceObject);
     });
 
     test('userRegistration function', () => {
         // Check if the user get's registered successfully.
+        registration(req, res);
     });
 });
